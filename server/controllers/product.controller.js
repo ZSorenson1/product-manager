0c061d0fe90d8c744519a15df651b9b89f52ea3f
Zachary Sorenson <zacharyjsorenson@gmail.com>
2020-11-10 14:56:22 -0500

    part 2 and 3

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,9 +1,22 @@
+const { request, response } = require('express');
 const { Product } = require('../models/product.model');
 
 module.exports.index = (request, response) => {
     response.json({
         message: "Hello World"
     });
+}
+
+module.exports.allProducts = (request, response) => {
+    Product.find({})
+    .then(allProds => response.json(allProds))
+    .catch(err => response.json({message: "Something went wrong", error: err}))
+}
+
+module.exports.findProduct = (request, response) => {
+    Product.findOne({_id: request.params.id})
+    .then(prod => response.json(prod))
+    .catch(err => response.json({message: "Something went wrong", error: err}))
 }
 
 module.exports.createProduct = (request, response) => {
@@ -15,4 +28,16 @@ module.exports.createProduct = (request, response) => {
     })
     .then(product => response.json(product))
     .catch(err => response.json(err));
+}
+
+module.exports.editProduct = (request, response) => {
+    Product.findOneAndUpdate({_id: request.params.id}, request.body, {new: true})
+    .then(prod => response.json(prod))
+    .catch(err => response.json(err))
+}
+
+module.exports.deleteProduct = (request, response) => {
+    Product.deleteOne({_id: request.params.id})
+    .then(res => response.json(res))
+    .catch(err => response.json(err))
 }
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,5 +2,9 @@ const ProductController = require('../controllers/product.controller');
 
 module.exports = function(app){
     app.get('/api' , ProductController.index);
-    app.post('/api/products', ProductController.createProduct)
+    app.get('/api/products', ProductController.allProducts);
+    app.get('/api/products/:id', ProductController.findProduct);
+    app.put('/api/products/:id/edit', ProductController.editProduct);
+    app.delete('/api/products/:id/delete', ProductController.deleteProduct);
+    app.post('/api/products/new', ProductController.createProduct);
 }
